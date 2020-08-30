e605539ffac12a0ed523f733adfb97073f8b89a4
weiyuyang <963369304@qq.com>
2020-08-30 15:28:28 +0800

    add 80 port

--- SC20200827/cloudconsumerorder80/src/main/java/com/nju/springcloud/entities/Payment.java
+++ SC20200827/cloudconsumerorder80/src/main/java/com/nju/springcloud/entities/Payment.java
@@ -13,4 +13,3 @@ public class Payment implements Serializable {
     private Long id;
     private String serial;
 }
-
--- SC20200827/payment8001/src/main/java/com/nju/springcloud/controller/PaymentController.java
+++ SC20200827/payment8001/src/main/java/com/nju/springcloud/controller/PaymentController.java
@@ -31,9 +31,9 @@ public class PaymentController {
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
-        log.info("*****查询结果："+payment);
+        log.info("*****查询结果："+payment+"无内鬼");
 
         if(payment!=null){
             return new CommonResult(200,"查询成功",payment);
