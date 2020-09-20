9a8f0f35a4a58200e12d33e39e5b1a2235d63158
Weizerojust <963369304@qq.com>
2020-09-20 17:24:55 +0800

    change`

--- SC20200827/cloud-provider-hystrix-payment8001/src/main/java/com/nju/springcloud/service/PaymentService.java
+++ SC20200827/cloud-provider-hystrix-payment8001/src/main/java/com/nju/springcloud/service/PaymentService.java
@@ -12,7 +12,7 @@ public class PaymentService {
      * @return
      */
     public String paymentInfo_OK(Integer id){
-        return "线程池： "+ Thread.currentThread().getName()+" payment_OK, id: "+"\t"+"bingo";
+        return "线程池： "+ Thread.currentThread().getName()+" payment_OK, id: "+id+"\t"+"bingo";
     }
 
     public String paymentInfo_Timeout(Integer id){
@@ -22,7 +22,7 @@ public class PaymentService {
         }catch (InterruptedException e){
             e.printStackTrace();
         }
-        return "线程池： "+ Thread.currentThread().getName()+" payment_Timeout, id: "+"\t"+"耗时: "+timeNumber+"s";
+        return "线程池： "+ Thread.currentThread().getName()+" payment_Timeout, id: "+id+"\t"+"耗时: "+timeNumber+"s";
     }
 }
 
